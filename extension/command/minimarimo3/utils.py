cbc13a9e7af7e2ca07a0f991d13a63d7f8b0eaf9
minimarimo3 <40518821+minimarimo@users.noreply.github.com>
2023-07-06 12:45:33 +0900

    chore: 基礎を作成

---
+++ extension/command/minimarimo3/utils.py
@@ -0,0 +1,13 @@
+
+def show_user_info() -> str:
+    """現在ユーザーに登録されている情報を全て表示します"""
+    return "OK?"
+
+
+def fill_name() -> str:
+    return "OK?"
+
+
+def fill_user_info(profile) -> str:
+    """ユーザーのプロフィールを埋めます"""
+    return "OK?"
