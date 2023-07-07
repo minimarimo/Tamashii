409406008612c5b7d6e42479533279a89ee4e9cd
minimarimo3 <40518821+minimarimo@users.noreply.github.com>
2023-07-08 05:35:14 +0900

    feat: コマンドwo

--- core/desktop_mascot_controller.py
+++ core/desktop_mascot_controller.py
@@ -1,10 +1,12 @@
+import random
 from dataclasses import asdict
 from threading import Thread
+from typing import Optional, Type
 
 from core.extension.prefernce.data import PreferenceData
 from core.extension.scenario.data import LoadedScenarioData
 from core.extension.prefernce.loader import load_hook
-from core.extension.scenario.loader import load_scenario
+from core.extension.scenario.loader import load_scenario, parse_scenario
 from core.messenger.messenger import Messenger
 from core.messenger.protocol import *
 
@@ -17,6 +19,7 @@ class DesktopMascotController:
     """拡張機能で実装された関数が格納されています"""
 
     def __init__(self, preference: PreferenceData):
+        self._is_debug = True
         DesktopMascotController.hooks = load_hook(preference.extension.hook)
         self._preference = preference
         """拡張のために実装された関数を格納する"""
@@ -24,6 +27,31 @@ class DesktopMascotController:
         # self._ui = load_ui(preference.core.ui)
         # self._model = load_model(preference.extension.model)
         self._scenario = load_scenario(preference.extension.scenario)
+        self._flags = {}
+        """karadaから送られてきたフラグを格納する"""
+        self.user_love_level = 50
+
+        # FIXME: 本来なら今実行中のキャラの設定が入るのですが、今はデバッグなのでこうしてます
+        self.my = self
+
+    def _action(self):
+        """
+        キャラが行動し、その結果を返します。
+        """
+        action_result: list
+        """キャラが行動を起こした結果です。"""
+        # karadaが何か検知していないかやタイマーからなんか来てないかなどを確認する
+        for flag in self._flags:
+            if flag in self._scenario.Message:
+                print(flag)
+                # TODO: ここでフラグに応じた処理を実行し、送信するstrを返す
+
+        # 何も起きていなければランダムにメッセージを送信する
+        on_random = self._scenario.Message.OnRandom
+        contents = on_random[random.randint(0, len(on_random) - 1)] if on_random else []
+        action_result = parse_scenario(contents, self)
+
+        return generate_say_command(SayArgs(message=action_result))
 
     def _run_sender(self):
         """
@@ -37,16 +65,20 @@ class DesktopMascotController:
             sender.read_str()
             # とりあえず1キャラだけロード TODO: 複数キャラ
             break
+
         while sender.is_available():
             # 本来ならここで何の行動をするか判断する実装が入る
             # 今は開発中なのでSayコマンドのみの実装
-            print(self._scenario)
-            message = input("喋る内容を入力してください:")
-            print("sender message: " + message)
+            message = self._action()
+            if not message:
+                continue
+            # message = input("喋る内容を入力してください:")
             # そう。ここが開発用のコードなので、SayArgsではなく適切なシナリオを読み込む関数に変わる
-            sender.write_str(generate_say_command(SayArgs([message])))
+            print("送信: " + str(message))
+            sender.write_str(message)
             response = sender.read_str()
             print("sender response: " + response)
+            input("Press Enter to continue...")
 
     def _run_receiver(self):
         """
